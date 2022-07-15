4ffff87d44ab493ac649649f6416a1dea59079fb
SaranPradeepJ <saranpradeep2003@gmail.com>
2022-07-15 10:28:29 +0530

    Homepage UI done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EventIcon from '@mui/icons-material/Event';
 import NavbarBoot from '../component/Navbar';
 import '../css/homeDash.css'
 import PercentIcon from '@mui/icons-material/Percent';
+import { useEffect } from 'react';
 function Homepage() {
+    const ServerUrl="http://localhost:5000"
+
+    //useStates 
+    const [events, setEvents] = useState([])
+    const [inCompletedEvents, setInCompletedEvents] = useState([])
+    
+    //useeffect for Server fetch
+    useEffect(() => {
+      fetch(ServerUrl+'/attendance/listOfEvents')
+    .then(res=>res.json())
+    .then(data=>setEvents(data))
+    }, [])
+    //
+    useEffect(() => {
+      fetch(ServerUrl+'/attendance/unCompleted')
+      .then(res=>res.json())
+      .then(data=>setInCompletedEvents(data))
+    }, [])
+    
+    
   return (
     <div className="Homepage">
         <NavbarBoot/>
@@ -19,16 +40,16 @@ function Homepage() {
                 <p className="totalText">
                     Total No of Events
                 </p>
-                <p className="totalNumber">36</p>
+                <p className="totalNumber">{events.count}</p>
             </div>
             <div className="totalNumberDet">
             <div className="totalNumberSub left">
                 <p className='totalText'>Completed</p>
-                <p className='totalNumber'>34</p>   
+                <p className='totalNumber'>{events.count-inCompletedEvents.unCompletedCount}</p>   
             </div>
             <div className="totalNumberSub">
                 <p className='totalText'>Upcoming</p>
-                <p className='totalNumber'>34</p>
+                <p className='totalNumber'>{inCompletedEvents.unCompletedCount}</p>
             </div>
             </div>            
         </div>
